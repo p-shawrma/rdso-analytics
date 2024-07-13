088362959adfecfb8ff1aa21577ad286a922a627
p-shawrma <157201518+p-shawrma@users.noreply.github.com>
2024-07-13 18:05:34 +0530

    Update vehicle_pack.py

--- pages/vehicle_pack.py
+++ pages/vehicle_pack.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
-import clickhouse_connect
 import numpy as np
 from datetime import datetime, timedelta
 import plotly.graph_objects as go
+import clickhouse_connect
 from pygwalker.api.streamlit import StreamlitRenderer
 
 # ClickHouse connection details
@@ -12,61 +12,56 @@ ch_user = 'default'
 ch_password = 'dKd.Y9kFMv06x'
 ch_database = 'telematics'
 
-# Function to create ClickHouse client
-def create_clickhouse_client():
+# Function to create a ClickHouse client
+def create_client():
     return clickhouse_connect.get_client(
         host=ch_host,
-        username=ch_user,
+        user=ch_user,
         password=ch_password,
         database=ch_database,
         secure=True
     )
 
-# Function to fetch all Model_Numb and DeviceDate for filters
-@st.cache(allow_output_mutation=True, ttl=6000, show_spinner=False)
-def fetch_all_model_date():
-    client = create_clickhouse_client()
-    query = "SELECT DISTINCT Model_Numb, DeviceDate FROM telematics.custom_tracking"
+# Function to fetch all model numbers and device dates
+@st.cache_data(ttl=6000)
+def fetch_model_numbers_and_dates():
+    client = create_client()
+    query = "SELECT DISTINCT Model_Numb, toStartOfDay(DeviceDate) as DeviceDate FROM custom_tracking"
     result = client.query(query)
     df = pd.DataFrame(result.result_rows, columns=result.column_names)
     return df
 
-# Function to fetch filtered data from ClickHouse based on selected Model_Numb and dates
-@st.cache(allow_output_mutation=True, ttl=6000, show_spinner=False)
-def fetch_filtered_data(model_numbers, start_date, end_date):
-    client = create_clickhouse_client()
-    model_numbers_str = ', '.join([f"'{model}'" for model in model_numbers])
+# Function to fetch data based on filters
+@st.cache_data(ttl=6000)
+def fetch_data(model_numbers, start_date, end_date):
+    client = create_client()
+    model_numbers_str = ','.join([f"'{model}'" for model in model_numbers])
     query = f"""
     SELECT *
-    FROM telematics.custom_tracking
-    WHERE Model_Numb IN ({model_numbers_str}) 
-    AND DeviceDate BETWEEN '{start_date}' AND '{end_date}';
+    FROM custom_tracking
+    WHERE Model_Numb IN ({model_numbers_str}) AND DeviceDate BETWEEN '{start_date}' AND '{end_date}';
     """
     result = client.query(query)
     df = pd.DataFrame(result.result_rows, columns=result.column_names)
     return df
 
+# Function to process data
 def process_data(df):
     df['timestamp'] = pd.to_datetime(df['DeviceDate'])
     df = df.sort_values(by='timestamp')
     df['time_diff'] = df['timestamp'].diff().dt.total_seconds()
     
-    # Handle the first time_diff NaN
     df['time_diff'].fillna(method='bfill', inplace=True)
 
-    # Base alpha for an expected time difference, e.g., 10 seconds
-    base_time_diff = 10  # Base time difference in seconds
-    base_alpha = 0.33    # Base alpha for smoothing
+    base_time_diff = 10
+    base_alpha = 0.33
 
-    # Adjust alpha based on actual time difference
     df['alpha'] = df['time_diff'].apply(lambda x: base_alpha / x * base_time_diff if x > 0 else base_alpha)
-    df['alpha'] = df['alpha'].clip(upper=0.66)  # Ensure alpha does not exceed 0.45
+    df['alpha'] = df['alpha'].clip(upper=0.66)
 
-    # Initialize the first current to the first actual current reading
     ema_current = df['BM_BattCurrent'].iloc[0]
     smoothed_currents = [ema_current]
 
-    # Apply dynamic EMA for current
     for i in range(1, len(df)):
         alpha = df['alpha'].iloc[i]
         current = df['BM_BattCurrent'].iloc[i]
@@ -74,53 +69,41 @@ def process_data(df):
         smoothed_currents.append(ema_current)
 
     df['Fitted_Current(A)'] = smoothed_currents
-
-    # Static EMA for voltage
     df['Fitted_Voltage(V)'] = df['BM_BattVoltage'].ewm(alpha=base_alpha).mean()
 
-    # Calculate voltage increase
     df['voltage_increase'] = df['Fitted_Voltage(V)'].diff() > 0.01
-        
-    # Calculate soc increase
     df['soc_increase'] = df['BM_SocPercent'].diff() > 0.01
 
-    # Calculate average pack temperature from all cell temperature columns
     cell_temp_columns = [col for col in df.columns if 'Cell_Temperature' in col]
     df['Pack_Temperature_(C)'] = df[cell_temp_columns].mean(axis=1)
 
-    # Define conditions and choices for states
     epsilon = 0.05
     conditions = [
-            ( df['voltage_increase'] | df['soc_increase'] ) & (df['Fitted_Current(A)'] > epsilon),  # Charging condition
-            (df['Fitted_Current(A)'] < -epsilon) & (~df['voltage_increase']),  # Discharging condition
-            abs(df['Fitted_Current(A)']) <= epsilon  # Idle condition
-        ]
+        (df['voltage_increase'] | df['soc_increase']) & (df['Fitted_Current(A)'] > epsilon),
+        (df['Fitted_Current(A)'] < -epsilon) & (~df['voltage_increase']),
+        abs(df['Fitted_Current(A)']) <= epsilon
+    ]
     choices = ['charge', 'discharge', 'idle']
     df['state'] = np.select(conditions, choices, default='idle')
 
-    # Group by state changes and filter short durations
     df['state_change'] = (df['state'] != df['state'].shift(1)).cumsum()
     grp = df.groupby('state_change')
     df['state_duration'] = grp['timestamp'].transform(lambda x: (x.max() - x.min()).total_seconds())
     df['filtered_state'] = np.where(df['state_duration'] > 5, df['state'], np.nan)
     df['filtered_state'].fillna(method='bfill', inplace=True)
 
-    # Map filtered_state to numerical step types
     state_mapping = {'charge': 0, 'discharge': 1, 'idle': 2}
     df['step_type'] = df['filtered_state'].map(state_mapping)
 
     return df
 
 def calculate_percentile(n):
-    """Utility function to create a percentile function for aggregation."""
     def percentile_(x):
         return np.percentile(x, n)
     percentile_.__name__ = 'percentile_%s' % n
     return percentile_
 
 def process_grouped_data(df):
-    """Processes grouped data to extract meaningful statistics for each continuous state segment."""
-    # Group by continuous states and apply calculations
     grouped = df.groupby((df['filtered_state'] != df['filtered_state'].shift()).cumsum())
     result = grouped.agg(
         start_timestamp=('timestamp', 'min'),
@@ -141,14 +124,10 @@ def process_grouped_data(df):
         median_min_cell_temperature=('Min_monomer_temperature', 'median'),
         median_pack_temperature=('Pack_Temperature_(C)', 'median')
     )
-    
-    # Calculate the 'date' from 'start_timestamp'
+
     result['date'] = result['start_timestamp'].dt.date
-    
-    # Calculate the change in SOC
     result['change_in_soc'] = result['soc_end'] - result['soc_start']
-    
-    # Reorder columns to place 'date' just before 'start_timestamp' and 'change_in_soc' just after 'soc_end'
+
     columns_ordered = ['date', 'start_timestamp', 'end_timestamp', 'step_type', 'duration_minutes',
                        'soc_start', 'soc_end', 'change_in_soc', 'voltage_start', 'voltage_end',
                        'average_current', 'median_current', 'min_current', 'max_current', 'current_25th',
@@ -159,7 +138,6 @@ def process_grouped_data(df):
     return result
 
 def apply_filters(df):
-    # Retrieve user-selected step types from the sidebar
     step_types = df['step_type'].unique().tolist()
     selected_types = st.sidebar.multiselect('Select Step Types', step_types, default=step_types)
     filtered_df = df[df['step_type'].isin(selected_types)]
@@ -216,7 +194,7 @@ def calculate_midpoint(row):
 
 def plot_discharge_currents(df):
     df['mid_timestamp'] = df.apply(calculate_midpoint, axis=1)
-    discharge_df = df[df['step_type'] == 1]  # 1 represents 'discharge'
+    discharge_df = df[df['step_type'] == 'discharge']
     fig = go.Figure(data=[go.Candlestick(
         x=discharge_df['mid_timestamp'],
         open=discharge_df['current_25th'],
@@ -229,26 +207,21 @@ def plot_discharge_currents(df):
     return fig
 
 def create_day_wise_summary(df):
-    # Filter the DataFrame for discharge and charge states
-    discharge = df[df['step_type'] == 1]  # 1 represents 'discharge'
-    charge = df[df['step_type'] == 0]  # 0 represents 'charge'
+    discharge = df[df['step_type'] == 'discharge']
+    charge = df[df['step_type'] == 'charge']
 
-    # Aggregate the data by date for discharge
     discharge_summary = discharge.groupby('date').agg({
         'change_in_soc': 'sum',
         'duration_minutes': ['sum', 'min', 'max', 'median', calculate_percentile(25), calculate_percentile(75)]
     })
 
-    # Aggregate the data by date for charge
     charge_summary = charge.groupby('date').agg({
         'change_in_soc': 'sum'
     })
 
-    # Flatten the MultiIndex columns created by aggregation
     discharge_summary.columns = ['_'.join(col).strip() for col in discharge_summary.columns.values]
     charge_summary.columns = ['total_charge_soc']
 
-    # Merge discharge and charge summaries into a single DataFrame
     day_wise_summary = pd.merge(discharge_summary, charge_summary, on='date', how='outer')
     day_wise_summary.rename(columns={
         'change_in_soc_sum': 'total_discharge_soc',
@@ -277,26 +250,24 @@ def plot_discharge_duration_candlestick(df):
 def main():
     st.set_page_config(layout="wide", page_title="Battery Discharge Analysis")
 
-    # Sidebar for date and filtering input
     with st.sidebar:
         st.title("Filter Settings")
-        filter_data = fetch_all_model_date()
-        model_numbers = filter_data['Model_Numb'].unique().tolist()
-        selected_models = st.multiselect('Select Model Numbers', model_numbers, default=model_numbers)
-        dates = filter_data['DeviceDate'].unique()
-        min_date = datetime.strptime(min(dates), '%Y-%m-%d')
-        max_date = datetime.strptime(max(dates), '%Y-%m-%d')
-        start_date = st.date_input("Start Date", min_date)
-        end_date = st.date_input("End Date", max_date)
+        model_numbers_and_dates = fetch_model_numbers_and_dates()
+        model_numbers = model_numbers_and_dates['Model_Numb'].unique().tolist()
+        selected_model_numbers = st.multiselect('Select Model Numbers', model_numbers, default=model_numbers)
+
+        date_range = model_numbers_and_dates['DeviceDate'].unique()
+        start_date = st.date_input("Start Date", min(date_range), min_value=min(date_range), max_value=max(date_range))
+        end_date = st.date_input("End Date", max(date_range), min_value=min(date_range), max_value=max(date_range))
+
         fetch_button = st.button("Fetch Data")
 
-    # Fetch data on button press
     if fetch_button or 'data_loaded' not in st.session_state:
         if start_date > end_date:
             st.error("End date must be after start date.")
             return
 
-        df = fetch_filtered_data(selected_models, start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d'))
+        df = fetch_data(selected_model_numbers, start_date, end_date)
         if not df.empty:
             processed_df = process_data(df)
             grouped_df = process_grouped_data(processed_df)
@@ -308,34 +279,31 @@ def main():
             st.session_state['data_loaded'] = False
 
     if st.session_state.get('data_loaded', False):
-        # Initialize filters after data is loaded
         all_step_types = st.session_state['grouped_df']['step_type'].unique().tolist()
         selected_step_types = st.multiselect('Select Step Type', all_step_types, default=all_step_types)
 
-        # Filter the data based on step type selection
         filtered_df = st.session_state['grouped_df'][st.session_state['grouped_df']['step_type'].isin(selected_step_types)]
 
-        # Update the duration slider based on filtered data
         if not filtered_df.empty:
             min_duration, max_duration = filtered_df['duration_minutes'].agg(['min', 'max'])
             min_duration, max_duration = int(min_duration), int(max_duration)
         else:
-            min_duration, max_duration = 0, 0  # Defaults when no data is available
+            min_duration, max_duration = 0, 0
         
-        # Set the initial value of the slider to start at 10, or at the minimum value if it's higher than 10
         initial_min_duration = max(1, min_duration)
         
         duration_range = st.slider("Select Duration Range (minutes)", min_duration, max_duration, (initial_min_duration, max_duration))
         
-        # Apply the duration filter
         filtered_df = filtered_df[(filtered_df['duration_minutes'] >= duration_range[0]) & (filtered_df['duration_minutes'] <= duration_range[1])]
-        # Display plots and data
+
         display_data_and_plots(filtered_df, st.session_state['processed_df'])
 
 def display_data_and_plots(filtered_df, processed_df):
     st.write("Data Overview:")
     st.dataframe(processed_df)
-   
+    
+    # Add vis spec here
+    
     fig = plot_current_voltage(processed_df)
     st.plotly_chart(fig, use_container_width=True)
     fig = plot_current_soc(processed_df)
@@ -357,5 +325,7 @@ def display_data_and_plots(filtered_df, processed_df):
     
     fig = plot_discharge_duration_candlestick(summary_df)
     st.plotly_chart(fig, use_container_width=True)
+
 if __name__ == "__main__":
     main()
+
